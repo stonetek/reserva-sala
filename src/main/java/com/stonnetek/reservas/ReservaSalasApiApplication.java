4b4f79fa1c2637bd4d847accb0b91a84523d5c8f
Pestrus Stone <pelkorstone@gmail.com>
2026-05-27 15:34:25 -0300

    Primeira versão salas para reserva

---
+++ src/main/java/com/stonnetek/reservas/ReservaSalasApiApplication.java
@@ -0,0 +1,13 @@
+package com.stonnetek.reservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaSalasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaSalasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stonnetek/reservas/ReservaSalasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stonnetek.reservas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaSalasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
